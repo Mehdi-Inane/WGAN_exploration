e5991d2baf5710c448fc12a90983574f06c4a22a
Mehdi <inaneahmedmehdi@gmail.com>
2023-11-06 14:21:14 +0100

    adding dropout and removing batch normalization

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
     parser.add_argument("--epochs", type=int, default=100,
                         help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=0.002,
+    parser.add_argument("--lr", type=float, default=0.0002,
                       help="The learning rate to use for training.")
     parser.add_argument("--batch_size", type=int, default=128, 
                         help="Size of mini-batches for SGD")
@@ -107,7 +107,7 @@ if __name__ == '__main__':
 
         if epoch % 10 == 0:
             save_models(G, D, 'checkpoints')
-        if epoch % 20 == 0:
+        if epoch % 10 == 0:
             real_images_path = 'data/MNIST_raw'
             generated_images_path = 'samples'
             generate_fake_samples(G, 10000)
