fb1179302c2219e627a93aca5b43953d73343ab7
iasd23_agervreau <iasd23_agervreau@boldeagle.nat.lamsade.dauphine.fr>
2023-11-15 14:08:39 +0100

    good good

--- train.py
+++ train.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
     parser.add_argument("--epochs", type=int, default=200,
                         help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=1e-4,
+    parser.add_argument("--lr", type=float, default=5e-4,
                       help="The learning rate to use for training.")
-    parser.add_argument("--batch_size", type=int, default=128, 
+    parser.add_argument("--batch_size", type=int, default=64, 
                         help="Size of mini-batches for SGD")
 
     args = parser.parse_args()
@@ -107,7 +107,7 @@ if __name__ == '__main__':
     fid_values = []
     D_loss = []
     G_loss = []
-    n_generator = 5
+    n_generator = 1
     z_fixed = torch.randn(1, 100)
     os.makedirs('samples_per_epoch', exist_ok=True)
     os.makedirs('samples_per_epoch_random', exist_ok=True)
